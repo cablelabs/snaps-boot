8de0d1d7923a9a732b572a3449e48b3de41fa8c4
spisarski <s.pisarski@cablelabs.com>
2018-09-19 15:04:06 -0600

    Stubs

--- snaps_boot/provision/hardware/digitalrebar/rebar_utils.py
+++ snaps_boot/provision/hardware/digitalrebar/rebar_utils.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
+import requests
 
 
 def setup_dhcp_service(boot_conf):
@@ -18,3 +20,39 @@ def setup_dhcp_service(boot_conf):
     Creates a DHCP service
     :param boot_conf: the configuration
     """
+    subnet_conf = __generate_subnet_config(boot_conf)
+    url = None
+    data = json.dumps(subnet_conf)
+    response = requests.post(url, data=data)
+
+
+def __generate_subnet_config(boot_conf):
+    """
+    Returns a dict containing the
+    :param boot_conf:
+    :return:
+    """
+    cidr = None
+    start = None
+    end = None
+    gateway = None
+    dns = None
+    domain = None
+
+    return """
+    {
+        "Name": "local_subnet",
+        "Subnet": "{0}",
+        "ActiveStart": "{1}",
+        "ActiveEnd": "{2}",
+        "ActiveLeaseTime": 60,
+        "Enabled": true,
+        "ReservedLeaseTime": 7200,
+        "Strategy": "MAC",
+        "Options": [
+            { "Code": 3, "Value": "{3}", "Description": "Default Gateway" },
+            { "Code": 6, "Value": "{4}", "Description": "DNS Servers" },
+            { "Code": 15, "Value": "{5}", "Description": "Domain Name" }
+        ]
+    }
+    """.format(cidr, start, end, gateway, dns, domain)
