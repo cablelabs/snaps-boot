e3955e5d450af9a4c92b7902a3e1ce2f7acc8a59
Bo Quan <b.quan@cablelabs.com>
2019-01-02 19:00:29 +0000

    Added exception handling when rebooting

--- snaps_boot/provision/ipmi_utils.py
+++ snaps_boot/provision/ipmi_utils.py
@@ -11,9 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import logging
 import time
 
 from pyghmi.ipmi.command import Command
+from pyghmi.exceptions import IpmiException
+
+logger = logging.getLogger('snaps_boot_ipmi_utils')
+#logger.setLevel(logging.DEBUG)
 
 
 def reboot_pxe(boot_conf):
@@ -35,9 +41,16 @@ def reboot_disk(boot_conf):
 def __reboot_all(boot_conf, boot_order):
     creds = get_ipmi_creds(boot_conf)
     for ip, user, password in creds:
-        command = Command(ip, user, password)
-        __set_boot_order(command, boot_order)
-        __reboot(command)
+        try:
+            command = Command(ip, user, password)
+            __set_boot_order(command, boot_order)
+            __reboot(command)
+        except IpmiException as e:
+            # print the error message and server bmc information
+            # will continue to reboot the rest of servers
+            logger.error(e)
+            logger.warn('Failed to reboot server with BMC IP: %s, user: %s', ip, user)
+            logger.debug('BMC password: %s', password)
 
 
 def get_ipmi_creds(boot_conf):
