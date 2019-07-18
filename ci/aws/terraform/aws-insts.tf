0289bd491ed446b1ee0c39bb3a2bf9cfd2de66c9
spisarski <s.pisarski@cablelabs.com>
2019-07-18 15:41:35 -0600

    Initial checkin for EC2 CI automation scripts. Currently a 2xlarge VM will get spawned then have kvm/libvirt installed. 3 networks and 2 VMs are getting started on the virtual host.

---
+++ ci/aws/terraform/aws-insts.tf
@@ -0,0 +1,42 @@
+# Copyright (c) 2019 Cable Television Laboratories, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# AWS EC2 Instances
+resource "aws_instance" "snaps-boot-build" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = aws_key_pair.snaps-boot-pk.key_name
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "snaps-boot-ci-build-${var.build_id}"
+  }
+
+  security_groups = [aws_security_group.snaps-boot.name]
+  associate_public_ip_address = true
+
+  # Used to ensure host is really up before attempting to apply ansible playbooks
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt install python -y"
+    ]
+  }
+
+  # Remote connection info for remote-exec
+  connection {
+    host = self.public_ip
+    type     = "ssh"
+    user     = var.sudo_user
+    private_key = file(var.private_key_file)
+  }
+}
