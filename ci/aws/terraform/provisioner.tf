9f0e997109e3ebec9af68ade77d5aa317e34012e
spisarski <s.pisarski@cablelabs.com>
2019-07-22 20:21:34 -0600

    Started 3 networks, 2 VMs on host VM and fixed some python dependency issues

--- ci/aws/terraform/provisioner.tf
+++ ci/aws/terraform/provisioner.tf
@@ -11,25 +11,107 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Call ansible scripts to setup and run snaps-boot
-resource "null_resource" "snaps-boot-build" {
+# Call ensure SSH key has correct permissions
+resource "null_resource" "snaps-boot-pk-setup" {
   provisioner "local-exec" {
     command = "chmod 600 ${var.private_key_file}"
   }
+}
+
+# Call ansible scripts to setup KVM
+resource "null_resource" "snaps-boot-kvm-setup" {
+  depends_on = [null_resource.snaps-boot-pk-setup]
+
+  # Install KVM dependencies
+  provisioner "local-exec" {
+    command = <<EOT
+${var.ANSIBLE_CMD} -u ${var.sudo_user} \
+-i ${aws_instance.snaps-boot-build.public_ip}, \
+${var.SETUP_KVM_DEPENDENCIES} \
+--key-file ${var.private_key_file} \
+EOT
+  }
+}
+
+# Call ansible scripts to setup KVM networks
+resource "null_resource" "snaps-boot-network-setup" {
+  depends_on = [null_resource.snaps-boot-kvm-setup]
+  # Create KVM networks
+  provisioner "local-exec" {
+    command = <<EOT
+${var.ANSIBLE_CMD} -u ${var.sudo_user} \
+-i ${aws_instance.snaps-boot-build.public_ip}, \
+${var.SETUP_KVM_NETWORKS} \
+--key-file ${var.private_key_file} \
+--extra-vars "\
+priv_ip_prfx=${var.priv_ip_prfx} \
+admin_ip_prfx=${var.admin_ip_prfx} \
+pub_ip_prfx=${var.pub_ip_prfx}
+"\
+EOT
+  }
+}
+
+# Call ansible scripts to setup KVM servers
+resource "null_resource" "snaps-boot-server-setup" {
+  depends_on = [null_resource.snaps-boot-network-setup]
 
   # Setup KVM on the VM to create VMs on it for testing snaps-boot
   provisioner "local-exec" {
     command = <<EOT
 ${var.ANSIBLE_CMD} -u ${var.sudo_user} \
 -i ${aws_instance.snaps-boot-build.public_ip}, \
-${var.SETUP_KVM_PB} \
+${var.SETUP_KVM_SERVERS} \
 --key-file ${var.private_key_file} \
 --extra-vars "\
-priv_ip_prfx=10.0.1 \
-admin_ip_prfx=10.0.2 \
-pub_ip_prfx=10.0.3
+aws_access_key=${var.access_key} \
+aws_secret_key=${var.secret_key} \
+pxe_img=/var/lib/libvirt/images/libvirt-pxe.qcow2
 "\
 EOT
   }
+}
 
+# Call ansible scripts to run snaps-boot
+resource "null_resource" "snaps-boot-deploy" {
+  depends_on = [null_resource.snaps-boot-server-setup]
+//  depends_on = [null_resource.snaps-boot-kvm-setup]
+
+  # Setup KVM on the VM to create VMs on it for testing snaps-boot
+  provisioner "local-exec" {
+    command = <<EOT
+${var.ANSIBLE_CMD} -u ${var.sudo_user} \
+-i ${aws_instance.snaps-boot-build.public_ip}, \
+${var.EXE_SNAPS_BOOT_PB} \
+--key-file ${var.private_key_file} \
+--extra-vars "\
+src_copy_dir=${var.src_copy_dir} \
+post_script_file=${var.post_script_file} \
+priv_ip_prfx=10.0.1 \
+admin_ip_prfx=10.0.2 \
+pub_ip_prfx=10.0.3
+ip_suffix_1=11
+ip_suffix_2=12
+ip_suffix_3=13
+admin_mac_1=foo-mac-1
+admin_mac_2=foo-mac-2
+admin_mac_3=foo-mac-3
+pub_gateway=foo-gateway
+broadcast_addr=foo-broadcast_addr
+domain_name=foo-domain_name
+dns_addr=8.8.8.8
+listen_iface=foo-listen_iface
+max_lease=7200
+netmask=foo-netmask
+router_ip=foo-router_ip
+build_admin_ip=foo-build_admin_ip
+http_proxy_port=3142
+priv_iface=eth0
+admin_iface=eth1
+admin_iface=eth2
+pxe_pass=foo-pxe_pass
+hosts_yaml_path=foo-hosts_yaml_path
+"\
+EOT
+  }
 }
--- ci/aws/terraform/variables.tf
+++ ci/aws/terraform/variables.tf
@@ -16,21 +16,35 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "build_id" {}
 
-# Optional Variables
-variable "public_key_file" {default = "../snaps-ci.pub"}
-variable "private_key_file" {default = "../snaps-ci"}
+# Variables that are recommended to change as they won't work in all envs
+variable "public_key_file" {default = "~/.ssh/id_rsa.pub"}
+variable "private_key_file" {default = "~/.ssh/id_rsa"}
+
+# Optional Variables for Cloud
 variable "sudo_user" {default = "ubuntu"}
 variable "region" {default = "us-west-2"}
 variable "availability_zone" {default = "us-west-2b"}
 
+# Optional Variables for test
+variable "src_copy_dir" {default = "/tmp"}
+# TODO - add in some logic into this default post_script file as it does nothing
+variable "post_script_file" {default = "/tmp/snaps-boot/ci/scripts/post_script"}
+variable "priv_ip_prfx" {default = "10.0.1"}
+variable "admin_ip_prfx" {default = "10.0.2"}
+variable "pub_ip_prfx" {default = "10.0.3"}
+
 # Ubuntu 16.04 SSD Volume Type
 //variable "ami" {default = "ami-0b37e9efc396e4c38"}
+# Ubuntu 18.04 SSD Volume Type
+variable "ami" {default = "ami-07b4f3c02c7f83d59"}
 
 # snaps-boot image with KVM and generic.qcow2
-variable "ami" {default = "ami-044440dc7a3d75d2b"}
+//variable "ami" {default = "ami-044440dc7a3d75d2b"}
 variable "instance_type" {default = "t2.2xlarge"}
 
 # Playbook Constants
 variable "ANSIBLE_CMD" {default = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook"}
-variable "SETUP_KVM_PB" {default = "../../playbooks/setup_kvm.yaml"}
+variable "SETUP_KVM_DEPENDENCIES" {default = "../../playbooks/kvm/dependencies.yaml"}
+variable "SETUP_KVM_NETWORKS" {default = "../../playbooks/kvm/networks.yaml"}
+variable "SETUP_KVM_SERVERS" {default = "../../playbooks/kvm/servers.yaml"}
 variable "EXE_SNAPS_BOOT_PB" {default = "../../playbooks/setup_build.yaml"}
